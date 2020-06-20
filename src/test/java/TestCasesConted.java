b160acd14635defc3c3a3f8ee3c374eac941cb1a
Serdar <serdar2225@gmail.com>
2020-06-19 22:41:07 -0400

    new database new test cases

--- src/test/java/TestCasesConted.java
+++ src/test/java/TestCasesConted.java
@@ -2,10 +2,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TestCasesConted {
     private Statement statement;
@@ -13,7 +10,7 @@ public class TestCasesConted {
 
     @BeforeClass
     public void connect() throws SQLException {
-        String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/serdar2225_students";
+        String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/serdar2225_students_newtest";
         String user = "serdar2225";
         String password = "serdar2225@hotmail.com";
         connection = DriverManager.getConnection(url, user, password);
@@ -28,6 +25,15 @@ public class TestCasesConted {
 
     @Test
     public void test() throws SQLException {
+        ResultSet rs = statement.executeQuery("SELECT first_name, city, country, postal_code FROM students_new;");
+
+        while (rs.next()) {
+            String name = rs.getString("first_name");
+            String country = rs.getString("country");
+            String city = rs.getString("city");
+            String postal_code = rs.getString("postal_code");
+            System.out.println(name + " " + country + " " + city + " " + postal_code);
+        }
 
     }
 
