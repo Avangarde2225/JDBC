205b6aa91f446db62dadb3f97825b9d77d161e46
Serdar <serdar2225@gmail.com>
2020-06-19 20:59:31 -0400

    reorganized the functions

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,18 +1,29 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.sql.*;
 
 public class TestCases {
-    //private Statement statement;
-    //private Connection connection;
+    private Statement statement;
+    private Connection connection;
+
+    @BeforeClass
+    public void connect() throws SQLException {
+        String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/serdar2225_students";
+        String user = "serdar2225";
+        String password = "serdar2225@hotmail.com";
+        connection = DriverManager.getConnection(url, user, password);
+        statement = connection.createStatement();
+
+    }
+    @AfterClass
+    public void disconnect() throws SQLException {
+        connection.close();
+    }
 
     @Test
     public void test() throws SQLException {
-    String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/serdar2225_students";
-    String user = "serdar2225";
-    String password = "serdar2225@hotmail.com";
-    Connection connection = DriverManager.getConnection(url, user, password);
-    Statement statement = connection.createStatement();
         ResultSet rs= statement.executeQuery("SELECT first_name, last_name, email FROM students;");
         while (rs.next()){
             String name =rs.getString(1);
