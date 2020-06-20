072904fb2cbc2a1996ad637d5d15bdfb67db25b1
Serdar <serdar2225@gmail.com>
2020-06-19 20:55:41 -0400

    all the students info - mock data

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,19 +1,24 @@
 import org.testng.annotations.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TestCases {
-    private Statement statement;
-    private Connection connection;
+    //private Statement statement;
+    //private Connection connection;
+
     @Test
     public void test() throws SQLException {
-    String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/daulet2030_studens_database";
+    String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/serdar2225_students";
     String user = "serdar2225";
     String password = "serdar2225@hotmail.com";
-    connection = DriverManager.getConnection(url, user, password);
-    statement = connection.createStatement();
+    Connection connection = DriverManager.getConnection(url, user, password);
+    Statement statement = connection.createStatement();
+        ResultSet rs= statement.executeQuery("SELECT first_name, last_name, email FROM students;");
+        while (rs.next()){
+            String name =rs.getString(1);
+            String lastName =rs.getString(2);
+            String email =rs.getString(3);
+            System.out.println(name + " " + lastName + " "+ email);
+        }
 }
 }
